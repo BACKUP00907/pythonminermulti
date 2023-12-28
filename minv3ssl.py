f56c496814a629f7dcb04885945eaa75cfb9d078
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-28 12:46:54 +0000

    ffgfh

--- minv3ssl.py
+++ minv3ssl.py
@@ -108,9 +108,9 @@ def controller(q,s,t,k):
 
         }
 
-        print('Logging into pool: {}:{}'.format(pool_host, pool_port))
+        #print('Logging into pool: {}:{}'.format(pool_host, pool_port))
 
-        print('Using NiceHash mode: {}'.format(nicehash))
+        #print('Using NiceHash mode: {}'.format(nicehash))
 
         s.sendall(str(json.dumps(login)+'\n').encode('utf-8'))
 
@@ -266,7 +266,7 @@ def worker(q, s):
 
             seed_hash = binascii.unhexlify(job.get('seed_hash'))
 
-            print('New job with target: {}, RandomX, height: {}'.format(target, height))
+            #print('New job with target: {}, RandomX, height: {}'.format(target, height))
 
             
 
@@ -284,7 +284,7 @@ def worker(q, s):
 
             xbin = binascii.unhexlify(blob)
 
-            print(len(blob))
+            #print(len(blob))
 
             fbin = struct.pack('39B', *bytearray(xbin[:39]))
 
@@ -342,7 +342,7 @@ def worker(q, s):
 
                 }
 
-                print('Submitting hash: {}'.format(hex_hash))
+                #print('Submitting hash: {}'.format(hex_hash))
 
             
 
